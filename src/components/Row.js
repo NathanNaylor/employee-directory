60a0c17fa7833c92567294d2f27c39a5064151b7
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-05-08 18:43:35 -0700

    posted user data to page

---
+++ src/components/Row.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Row(props) {
+  return (
+    <div>
+      <ul>
+        {props.results.map((result) => (
+          <li className="list-group-item" key={result.cell}>
+            {result.name.first} {result.name.last}
+          </li>
+        ))}
+      </ul>
+
+      <ul>
+        {props.results.map((result) => (
+          <li className="list-group-item" key={result.cell}>
+            {result.gender}
+          </li>
+        ))}
+      </ul>
+
+      <ul>
+        {props.results.map((result) => (
+          <li className="list-group-item" key={result.cell}>
+            {result.location.country}
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {props.results.map((result) => (
+          <li className="list-group-item" key={result.cell}>
+            <img
+            alt={result.name.first}
+            className="img-fluid"
+            src={result.picture.thumbnail}
+          />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Row;
