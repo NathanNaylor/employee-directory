a5cce642be3abeba1187aeaeaec28e356926a9cd
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-05-08 21:04:50 -0700

    changed some file structures

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 import "./App.css";
 import Table from "./components/Table";
 
+
 function App() {
-  return <Table />;
+
+  return (
+    <Table/>
+  );
 }
 
 export default App;
